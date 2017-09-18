50b627cf73ae6a3e5ada916c1b48dd57d4907921
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2017-09-18 15:44:22 -0500

    Rubocop clean

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ require 'timecop'
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
 
